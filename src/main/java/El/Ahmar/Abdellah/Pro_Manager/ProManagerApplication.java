bea32cea8859f4e73bc30648ff1f710d6e8ce1de
AbdellahElahmar <elahmar.abdellah@gmail.com>
2025-02-09 12:53:14 +0100

    Premier commit de mon projet

---
+++ src/main/java/El/Ahmar/Abdellah/Pro_Manager/ProManagerApplication.java
@@ -0,0 +1,13 @@
+package El.Ahmar.Abdellah.Pro_Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/El/Ahmar/Abdellah/Pro_Manager/ProManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package El.Ahmar.Abdellah.Pro_Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
